9b3ecb214b8731756ff8c4411dd78aec716766a1
Lachyx <lachyxx@proton.me>
2024-04-06 19:20:49 +1100

    Update

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # Nitrado Ark API Wrapper
 
 Made this because I was bored
+
+https://github.com/Lachyx/Nitrado-Ark-API-Wrapper
+
+
+# Example Code
+
+```javascript
+const { GameService, PlayerService  } = require('nitrado-ark-api-wrapper')
+
+const Player = new PlayerService('Auth')
+const Service = new GameService('Auth')
+
+try {
+    Player.Ban('XXXXX', 313123)
+} catch (err) {
+    console.log(err)
+};
+
+```
--- tests/script.js
+++ tests/script.js
@@ -3,12 +3,8 @@ const { GameService, PlayerService  } = require('nitrado-ark-api-wrapper')
 const Player = new PlayerService('Auth')
 const Service = new GameService('Auth')
 
-Player.Ban('XXXXX', 313123)
-Player.Unban('XXXXX', 313123)
-
-Service.Banlist('XXXXX')
-Service.List('XXXXX')
-Service.Start('XXXXX', 'Log Message', 'Restart Message')
-Service.Stop('XXXXX', 'Log Message', 'Stop Message')
-Service.Restart('XXXXX', 'Log Message', 'Restart Message')
-Service.Players('XXXXX')
+try {
+    Player.Ban('XXXXX', 313123)
+} catch (err) {
+    console.log(err)
+};
